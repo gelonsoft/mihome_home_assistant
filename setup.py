9795d4c3bcd9cc2f41b45d314fec592c96ba383b
gelonsoft <gelonsoft@gmail.com>
2021-02-15 00:38:49 +0300

    Fix requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mihome_home_assistant',
+    version='0.0.1',
+    packages=['custom_components', 'custom_components.gelonsoftmihome', 'custom_components.gelonsoftmihome.api',
+              'custom_components.gelonsoftmihome.api.devices'],
+    url='https://github.com/gelonsoft/mihome_home_assistant',
+    license='MIT',
+    author='gelonsoft',
+    author_email='gelonsoft@gmail.com',
+    description=''
+)
